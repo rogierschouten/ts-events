8f947cdb37048eb05d5bca131da9430fa7cfae52
Rogier Schouten <r.schouten@spiritit.com>
2015-04-29 13:36:21 +0200

    Removed example .d.ts files

--- build.sh
+++ build.sh
@@ -4,8 +4,18 @@ echo cleaning...
 rm -Rf lib/**/*.d.ts
 rm -Rf lib/**/*.js
 rm -Rf lib/**/*.map
+rm -Rf examples/**/*.d.ts
+rm -Rf examples/**/*.js
+rm -Rf examples/**/*.map
+rm -Rf test/**/*.d.ts
+rm -Rf test/**/*.js
+rm -Rf test/**/*.map
+rm -f index.d.ts
+rm -f index.js
+rm -f index.js.map
 rm -Rf doc/
 rm -Rf coverage/
+
 echo linting...
 for i in ./lib/*.ts ./index.ts; do
 	if [[ $i != *.d.ts ]]; then
