f9b415bf12512c0e6258e615ce90801c09e8d101
Rogier Schouten <r.schouten@spiritit.com>
2015-04-29 13:14:12 +0200

    Fixed missing typings files.

---
+++ typings/test.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./index.d.ts"/>
+/// <reference path="./chai/chai.d.ts"/>
+/// <reference path="./mocha/mocha.d.ts"/>
+/// <reference path="./source-map-support/source-map-support.d.ts"/>
